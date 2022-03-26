7d059e8cb4d10951af6cc012c6d646d253b35d39
ruchen <ruchen.gs@alibaba-inc.com>
2022-03-26 11:56:40 +0800

    add notes

--- core/src/test/java/com/gson/keno/lucene/nrt/NearRealTimeSearchDemo.java
+++ core/src/test/java/com/gson/keno/lucene/nrt/NearRealTimeSearchDemo.java
@@ -155,4 +155,46 @@ public class NearRealTimeSearchDemo {
             searcherManager.release(indexSearcher);
         }
     }
+
+    @Test
+    public void testOpenIfChange() throws IOException {
+        RAMDirectory ramDirectory = new RAMDirectory();
+        Document document = new Document();
+        document.add(new TextField("title", "Doc1", Field.Store.YES));
+        document.add(new IntPoint("ID", 1));
+        IndexWriter indexWriter = new IndexWriter(ramDirectory, new IndexWriterConfig(new StandardAnalyzer()));
+        indexWriter.addDocument(document);
+        indexWriter.commit();
+        DirectoryReader directoryReader = DirectoryReader.open(ramDirectory);
+        IndexSearcher indexSearcher = new IndexSearcher(directoryReader);
+        document = new Document();
+        document.add(new TextField("title", "Doc2", Field.Store.YES));
+        document.add(new IntPoint("ID", 2));
+        indexWriter.addDocument(document);
+        //即使commit，搜索依然不可见，需要重新打开reader
+        indexWriter.commit();
+        //只能看到Doc1
+        int count = indexSearcher.count(new MatchAllDocsQuery());
+        if (count > 0) {
+            TopDocs topDocs = indexSearcher.search(new MatchAllDocsQuery(), count);
+            for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+                System.out.println(indexSearcher.doc(scoreDoc.doc));
+            }
+        }
+        System.out.println("=================================");
+        //如果发现有新数据更新，则会返回一个新的reader
+        DirectoryReader newReader = DirectoryReader.openIfChanged(directoryReader);
+        if (newReader != null) {
+            indexSearcher = new IndexSearcher(newReader);
+            directoryReader.close();
+        }
+        count = indexSearcher.count(new MatchAllDocsQuery());
+        if (count > 0) {
+            TopDocs topDocs = indexSearcher.search(new MatchAllDocsQuery(), count);
+            for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+                System.out.println(indexSearcher.doc(scoreDoc.doc));
+            }
+        }
+        System.out.println("=================================");
+    }
 }
