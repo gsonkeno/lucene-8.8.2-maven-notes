884e3871435757f87299dbbededc70c31a641857
ruchen <ruchen.gs@alibaba-inc.com>
2022-10-31 16:43:21 +0800

    add notes

--- core/src/test/java/com/gson/keno/lucene/IndexSearcherTest.java
+++ core/src/test/java/com/gson/keno/lucene/IndexSearcherTest.java
@@ -20,9 +20,7 @@ import java.util.List;
 
 public class IndexSearcherTest {
     private String indexDir = new File(System.getProperty("user.dir")).getParentFile() + "/gsdata";
-
     private Directory directory;
-
     @Before
     public void before(){
         try {
@@ -30,55 +28,64 @@ public class IndexSearcherTest {
              FileOperation.deleteFile(indexDir);
              directory = new MMapDirectory(Paths.get(indexDir));
         } catch (IOException e) {
-            e.printStackTrace();
             throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void testFieldStored() throws IOException {
+        Analyzer analyzer = new WhitespaceAnalyzer();
+        IndexWriterConfig conf = new IndexWriterConfig(analyzer);
+        // 测试append模式
+        // conf.setOpenMode(IndexWriterConfig.OpenMode.APPEND);
+
+        conf.setUseCompoundFile(false);
+
+        IndexWriter indexWriter = new IndexWriter(directory, conf);
+        Document doc = new Document();
+        doc.add(new TextField("title", "how lucene work", Field.Store.YES));
+        doc.add(new TextField("author", "Mr Yang", Field.Store.NO));
+
+        indexWriter.addDocument(doc);
+        // 执行commit()操作后，生成segments_1文件
+        indexWriter.commit();
+
+        // indexWriter关闭，释放索引文件锁
+        indexWriter.close();
+
+        // ==============开始读=================
+        IndexReader reader = DirectoryReader.open(directory);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        // 查询命中term的文档
+        Query query = new TermQuery(new Term("author", "Yang"));
+        TopDocs topDocs = searcher.search(query, 1);
+        ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+        for(ScoreDoc scoreDoc : scoreDocs){
+            StringBuilder sb = new StringBuilder().append("<===docId = " + scoreDoc.doc);
+            Document d = searcher.doc(scoreDoc.doc);
+            List<IndexableField> fields = d.getFields();
+            for(IndexableField field : fields){
+                sb.append(", ").append(field.name()).append(" = ").append(field.stringValue());
+            }
+            sb.append("===>");
+            // print <===docId = 0, title = how lucene work===>
+            System.out.println(sb);
         }
     }
     @Test
     public void testSearchDocValues() throws IOException {
-//        FieldType type = new FieldType();
-//        type.setStored(true);
-//        type.setStoreTermVectors(true);
-//        type.setStoreTermVectorPositions(true);
-//        type.setStoreTermVectorPayloads(true);
-//        type.setStoreTermVectorOffsets(true);
-//        type.setTokenized(true);
-//        type.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS);
-//        type.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS);
         Analyzer analyzer = new WhitespaceAnalyzer();
         IndexWriterConfig conf = new IndexWriterConfig(analyzer);
         // 测试append模式
         // conf.setOpenMode(IndexWriterConfig.OpenMode.APPEND);
 
         IndexWriter indexWriter = new IndexWriter(directory, conf);
-        int count = 0;
         Document doc = new Document();
-        while (count++ < 3) {
-            // 文档0
-            doc.add(new TextField("author", "aab b aab aabbcc" + count,  Field.Store.YES));
-            doc.add(new TextField("content", "a b" + count,  Field.Store.YES));
-            doc.add(new IntPoint("intPoitn", 3, 4, 6));
-            indexWriter.addDocument(doc);
+        doc.add(new TextField("title", "how lucene work", Field.Store.NO));
 
-            // 文档1
-            doc = new Document();
-            doc.add(new TextField("author", "a" + count, Field.Store.YES));
-            doc.add(new TextField("content", "a b c h" + count, Field.Store.YES));
-            doc.add(new TextField("title", "d a" + count, Field.Store.YES));
-            doc.add(new NumericDocValuesField("sortByNumber", -1));
-            doc.add(new IntPoint("intPoitn", 3, 5, 6));
-            indexWriter.addDocument(doc);
-
-            // 文档2
-            doc = new Document();
-            doc.add(new TextField("author", "aab aab aabb" + count, Field.Store.YES));
-            doc.add(new TextField("content", "a c b e" + count, Field.Store.YES));
-            doc.add(new NumericDocValuesField("sortByNumber", 4));
-            indexWriter.addDocument(doc);
-            indexWriter.flush();
-            // 执行commit()操作后，生成segments_1文件
-            indexWriter.commit();
-        }
+        indexWriter.addDocument(doc);
+        // 执行commit()操作后，生成segments_1文件
+        indexWriter.commit();
 
         // indexWriter关闭，释放索引文件锁
         indexWriter.close();
